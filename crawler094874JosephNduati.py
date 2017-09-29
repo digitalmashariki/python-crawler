5decc33df70b72fccd1f1056567cc78cf615d73e
digitalmashariki <mungainduati@gmail.com>
2017-09-29 18:28:20 +0300

    Update crawler094874JosephNduati.py

--- crawler094874JosephNduati.py
+++ crawler094874JosephNduati.py
@@ -1,35 +1,31 @@
 
 # print(sys.version_info)
-#Runs with python 2.7 as default, incase of python 3.6 I will uncomment and comment as necessary
+#Runs with python 2.7 and  python 3.6 as default. I will uncomment and comment as necessary
 
 #Python 2.7
-import urllib
+#import urllib
 
 #OR
 
 #if Python 3.6
-#from urllib.request import urlopen 
+from urllib.request import urlopen 
 
 
 
 #if using python 2.7
-theURLObject = urllib.urlopen('https://www.strathmore.edu/')
+#theURLObject = urllib.urlopen('https://www.strathmore.edu/')
 
 #OR
 
 #I using python 3.6
-#theURLObject = urlopen('https://www.strathmore.edu/')
+theURLObject = urlopen('https://www.strathmore.edu/')
 
 theHTML = theURLObject.read()
 
 
 #Python 3.6
 #Convert the byte object to string
-#theHTMLAsString = theHTML.decode(("utf-8"))
-
-# print(type(theHTML.decode("utf-8")))
-
-
+theHTMLAsString = theHTML.decode(("utf-8"))
 
 myCounter = 0
 while myCounter < len(theHTMLAsString):
